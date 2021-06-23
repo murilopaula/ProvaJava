8ff771801a73aff5626e23d849dbd48d5dace097
Alexandre Ribeiro <xanderib@yahoo.com.br>
2021-06-23 19:50:56 -0300

    atualização

--- src/controllers/Util.java
+++ src/controllers/Util.java
@@ -22,5 +22,18 @@ public class Util {
 		
 		return valor;
 	}
-	
+	public static double lerDouble(String msg) {
+		double valor = 0;
+		boolean valorLido = false;
+		do {
+			try {
+				System.out.println(msg);
+				valor = Double.parseDouble(sc.next());
+				valorLido = true;
+			} catch (NumberFormatException e) {
+				System.out.println("\nApenas numeros\n");
+			}
+		}while(!valorLido);
+		return valor;
+	}	
 }
